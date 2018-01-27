299255f6d2b7dd2861e0df0d64a1f302642a1bff
Marcus Vinicius Bianchi dos Santos <marcuss@spi.local>
2018-01-27 12:48:56 -0200

    Changes for Production Enviroment.

--- Startup.cs
+++ Startup.cs
@@ -2,58 +2,55 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using historianproductionservice.Data;
+using historianproductionservice.Service;
+using historianproductionservice.Service.Interface;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
-using Microsoft.EntityFrameworkCore;
-using historianproductionservice.Data;
-using historianproductionservice.Service;
-using historianproductionservice.Service.Interface;
 
-namespace historianproductionservice
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
+namespace historianproductionservice {
+    public class Startup {
+        public Startup (IConfiguration configuration) {
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddSingleton<IConfiguration>(Configuration);
-            services.AddTransient<IOrderService, OrderService>();
-            services.AddTransient<IProductsService, ProductService>();
-            services.AddSingleton<IProductionOrderService, ProductionOrderService>();
+        public void ConfigureServices (IServiceCollection services) {
+            services.AddSingleton<IConfiguration> (Configuration);
+            services.AddTransient<IOrderService, OrderService> ();
+            services.AddTransient<IProductsService, ProductService> ();
+            services.AddSingleton<IProductionOrderService, ProductionOrderService> ();
 
-            services.AddCors(o => o.AddPolicy("CorsPolicy", builder =>
-                {
-                    builder.AllowAnyOrigin()
-                           .AllowAnyMethod()
-                           .AllowAnyHeader();
-                }));
-            services.AddDbContext<ApplicationDbContext>(options =>
-               options.UseNpgsql(Configuration.GetConnectionString("HistorianProductionDB")));
-            services.AddMvc();
+            services.AddCors (o => o.AddPolicy ("CorsPolicy", builder => {
+                builder.AllowAnyOrigin ()
+                    .AllowAnyMethod ()
+                    .AllowAnyHeader ();
+            }));
+            services.AddDbContext<ApplicationDbContext> (options =>
+                options.UseNpgsql (Configuration.GetConnectionString ("HistorianProductionDB")));
+            services.AddMvc ();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
-        {
-            app.UseCors("CorsPolicy");
-
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
+        public void Configure (IApplicationBuilder app, IHostingEnvironment env) {
+            app.UseResponseCaching ();
+            app.UseCors ("CorsPolicy");
+            app.UseForwardedHeaders (new ForwardedHeadersOptions {
+                ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+            });
+            if (env.IsDevelopment ()) {
+                app.UseDeveloperExceptionPage ();
             }
 
-            app.UseMvc();
+            app.UseMvc ();
         }
     }
 }
