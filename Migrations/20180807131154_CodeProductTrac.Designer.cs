b3efe1b225546918b02a8a9010a88e8aba2d8b9c
Ricardo Vieira Amorim dos Santos <ricardos@spi.local>
2018-08-07 15:25:51 -0300

    CampoCode

--- Migrations/20180807131154_CodeProductTrac.Designer.cs
+++ Migrations/20180807131154_CodeProductTrac.Designer.cs
@@ -11,9 +11,10 @@ using System;
 namespace historianproductionservice.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20180807131154_CodeProductTrac")]
+    partial class CodeProductTrac
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20180807131154_CodeProductTrac.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace historianproductionservice.Migrations
+{
+    public partial class CodeProductTrac : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "code",
+                table: "ProductTraceabilities",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "code",
+                table: "ProductTraceabilities");
+        }
+    }
+}
